8a8936710053955c578201ec524877a3bea91cbf
Charlie Cheever <ccheever@gmail.com>
2015-05-12 06:08:02 -0700

    Oops, forgot asyncs in the signatures

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ module.exports = {
   activeInterfaceAsync: function () { return network.promise.get_active_interface(); },
   gatewayIpAsync: function () { return network.promise.get_gateway_ip(); },
   interfacesListAsync: function () { return network.promise.get_interfaces_list(); },
-  privateIp: function () { return network.promise.get_private_ip(); },
-  publicIp: function () { return network.promise.get_public_ip(); },
+  privateIpAsync: function () { return network.promise.get_private_ip(); },
+  publicIpAsync: function () { return network.promise.get_public_ip(); },
 };
